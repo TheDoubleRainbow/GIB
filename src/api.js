7179dba4d902cacd0cfb9920f8046628b92c52d1
BuildTools <unconfigured@null.spigotmc.org>
2017-11-03 17:53:08 +0200

    ff

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-require('./lib/axios/axios');
+require('./lib/axios/axios.js');
 
 axios.get('https://api.github.com/repos/TheDoubleRainbow/GIB/issues')
   .then(function (response) {
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 Vue = require("./lib/vue.js");
 require("./app.vue");
+require("./api.js");
