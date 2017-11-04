3fc322a0d3f9567d6c338e8e51e13c26a7f7f5a9
Vlad Martyshev <vlad.martyshev@gmail.com>
2017-11-04 02:00:46 +0200

    Labels dropdown

--- src/labels.vue
+++ src/labels.vue
@@ -1,12 +1,32 @@
 Vue.component('labels', {
 	template: `<div><b>Labels:</b>
 					<ul>
-						<li v-for="label in labels"> {{label}}</li>
+						<li v-on:click="toogle($event)" v-for="label in labels">
+							<span>{{label.type}}</span>
+							<ul class = "sub-types">
+								<li v-for="subtype in label.subtypes">{{subtype}}</li>
+							</ul>
+						</li>
 					</ul>
 			</div>`,
 	data: function(){
 		return {
-			labels: ["Bug", "Feature", "SupDvach", "Ya lampovaya Nyasha"]
+			labels: 
+				[
+					{type: "Bug", subtypes: ["Bug1", "Bug2"]}, 
+					{type: "Feature", subtypes: ["Feature1", "Feature2"]},
+					{type: "SupDvach", subtypes: ["SupDvach1", "SupDvach2"]}
+				]
 		}
+	},
+	methods:{
+		toogle: function(event){
+			var subMenu = event.target.parentNode.querySelectorAll('.sub-types')[0];
+	        if (subMenu.classList.contains('selected')) {
+	            subMenu.classList.remove("selected");
+	        } else {
+	            subMenu.classList.add("selected");
+	        }
+	    }
 	}
 })
