903484191fdbd41b89bedd26f0453759e40c773b
TheDoubleRainbow <16mb.com@gmail.com>
2017-11-04 20:00:37 +0200

    chat animation

--- src/chat.vue
+++ src/chat.vue
@@ -2,7 +2,7 @@ Vue.component('chat', {
 	template: `
 		<div>
 			<div class="chat-open" @click="openChat()">Open chat</div>
-			<div class="chat-body" :id="chatid">
+			<div class="chat-body animated zoomInUp" :id="chatid">
 				<a @click="closeChat" class="delete chat-close"></a>
 				<div class="chat-display">
 					<div :class="getMessageType(message)" v-for="message in messages">{{message.user + ': ' + message.text}}</div>
@@ -25,15 +25,16 @@ Vue.component('chat', {
 			return message.user == "Me" ? "chat-message chat-message-byuser" : "chat-message"
 		},
 		openChat(){
-			document.getElementById(this.chatid).style.display = "block"; document.getElementById(this.chatid).style.left = window.innerWidth/2+'';
+			document.getElementById(this.chatid).className = "chat-body animated zoomInUp"; document.getElementById(this.chatid).style.display = "block"; document.getElementById(this.chatid).style.left = window.innerWidth/2+'';
 
 		},
 		closeChat(){
-			document.getElementById(this.chatid).style.display = "none";
+			document.getElementById(this.chatid).className = "chat-body animated zoomOutUp"
+			//document.getElementById(this.chatid).style.display = "none";
 		},
 		sendMessage(){
 			this.messages.push({user: "Me", text: this.input})
-			console.log(this.messages)
+			this.input = ""
 		}
 	}
 })
