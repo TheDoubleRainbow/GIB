4c7a6c575ed5b0dd0194152d8b688bde693f71d5
TheDoubleRainbow <16mb.com@gmail.com>
2017-11-05 01:31:45 +0200

    reviews

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -19628,10 +19628,10 @@ new Vue({
 
 Vue.component('reviews', {
 	template: `<div id = "reviews" class = "columns is-centered">
-					<div class = "column is-two-thirds-desktop">
+					<div class = "column is-10-widescreen is-10-fullhd is-12-desktop">
 						<div class = "columns is-centered">
-							<div class = "column is-9">
-								You are now looking at {{repo.repo}} repo by {{repo.member}}. This repo has 3 reviews.
+							<div class = "column is-7">
+								You are looking at {{repo.repo}} repo by {{repo.member}}. This repo has 3 reviews.
 							</div>
 							<div class = "is-1">
 								<div class = "control">
@@ -19639,10 +19639,38 @@ Vue.component('reviews', {
 								</div>
 							</div>
 						</div>
+						<div id="reviews-list" class="columns is-centered">
+							<div class="column is-10">
+								<div class="review" v-for="review in reviews">
+									<div class="review-header">
+										Review by {{review.user.name}} 
+									</div>
+									<div class="review-body">
+										{{review.text}}
+									</div>
+								</div>
+								<div class="review-new">
+									<div class="review-new-header">
+										Write your own review:
+									<div>
+									<div class="review-new-body column is-12">
+										<textarea rows="2" class="textarea"></textarea>
+									</div>
+									<div class="review-new-confirm">
+										<button class="button is-primary">Push my review</button>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>`,
 	data: function() {
 		return {
+			reviews: [
+				{id: 0, user: {name: "Vasya"}, text: "What a nice repo, i rly rly rly rly love it"},
+				{id: 1, user: {name: "Facebook"}, text: "This is our repo, normies get out reeeeeeeeê"},
+				{id: 2, user: {name: "Kolya"}, text: "Hello world"}
+			]
 		}
 	},
 	watch: {
@@ -19654,7 +19682,7 @@ Vue.component('reviews', {
 	
 	methods:{
 		loadViews: function(){
-			this.repoData = this.repo
+			
 		}
 	}
 })
--- src/reviews.vue
+++ src/reviews.vue
@@ -1,9 +1,9 @@
 Vue.component('reviews', {
 	template: `<div id = "reviews" class = "columns is-centered">
-					<div class = "column is-two-thirds-desktop">
+					<div class = "column is-10-widescreen is-10-fullhd is-12-desktop">
 						<div class = "columns is-centered">
-							<div class = "column is-9">
-								You are now looking at {{repo.repo}} repo by {{repo.member}}. This repo has 3 reviews.
+							<div class = "column is-7">
+								You are looking at {{repo.repo}} repo by {{repo.member}}. This repo has 3 reviews.
 							</div>
 							<div class = "is-1">
 								<div class = "control">
@@ -11,10 +11,38 @@ Vue.component('reviews', {
 								</div>
 							</div>
 						</div>
+						<div id="reviews-list" class="columns is-centered">
+							<div class="column is-10">
+								<div class="review" v-for="review in reviews">
+									<div class="review-header">
+										Review by {{review.user.name}} 
+									</div>
+									<div class="review-body">
+										{{review.text}}
+									</div>
+								</div>
+								<div class="review-new">
+									<div class="review-new-header">
+										Write your own review:
+									<div>
+									<div class="review-new-body column is-12">
+										<textarea rows="2" class="textarea"></textarea>
+									</div>
+									<div class="review-new-confirm">
+										<button class="button is-primary">Push my review</button>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>`,
 	data: function() {
 		return {
+			reviews: [
+				{id: 0, user: {name: "Vasya"}, text: "What a nice repo, i rly rly rly rly love it"},
+				{id: 1, user: {name: "Facebook"}, text: "This is our repo, normies get out reeeeeeeeê"},
+				{id: 2, user: {name: "Kolya"}, text: "Hello world"}
+			]
 		}
 	},
 	watch: {
@@ -26,7 +54,7 @@ Vue.component('reviews', {
 	
 	methods:{
 		loadViews: function(){
-			this.repoData = this.repo
+			
 		}
 	}
 })
