50974f7300bcfa5b12979a08b814ca87ce092cdd
TheDoubleRainbow <16mb.com@gmail.com>
2017-11-06 16:52:05 +0200

    axios fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 Vue = require("./lib/vue.js");
 Router = require("./lib/vue-router.min.js");
 io = require("./lib/socket.io.js");
-axios = require("./lib/axios/axios.js");
+//axios = require("./lib/axios/axios.js");
 require("./api.js");
 require("./reviews.vue")
 require("./chat.vue");
